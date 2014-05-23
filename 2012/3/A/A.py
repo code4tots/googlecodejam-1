19d3c9467555d083fcac72b1c60dcfae3417ed4a
math4tots <math4tots@gmail.com>
2014-05-23 13:55:14 -0700

    2012 3 A

---
+++ 2012/3/A/A.py
@@ -0,0 +1,20 @@
+# WATCH OUT FOR FLOATING POINT!!
+# My original solution involved calculating key as L[i]/1-P[i].
+# Works for small input, but generates wrong answer for large input!!
+class Key:
+	def __init__(self,ilp):
+		i, lp = ilp
+		l, p = map(int,lp)
+		self.i = i
+		self.l = l
+		self.p = 100 - p
+	
+	def __lt__(self,other):
+		return 100 * self.l + self.p * other.l < 100 * other.l + other.p * self.l
+	
+	def __repr__(self):
+		return str(self.i)
+
+for t in range(1,int(input())+1):
+	int(input())
+	print('Case #%d: %s' % (t,' '.join(map(str,sorted(map(Key,enumerate(zip(input().split(),input().split()))))))))
